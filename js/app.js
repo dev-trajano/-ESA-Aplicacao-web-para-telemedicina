9d1f1af45c25bdb652edcbc9a3c4a1fa9db4e82f
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-11-29 14:53:43 -0300

    autenticação firebase

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAvclsQAi4jF3VVKVsuN2pO9yDgJbatGc0',
+  authDomain: 'telemedicina-ceefa.firebaseapp.com',
+  atabaseURL: 'https://entendaantes.firebaseio.com',
+  projectId: 'telemedicina-ceefa',
+  storageBucket: 'telemedicina-ceefa.appspot.com',
+  messagingSenderId: '871401119286',
+  appId: '1:871401119286:web:cf16d6f22c1177cf770297',
+  measurementId: 'G-PEM0WHTYJ1'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
